21042cbdcc963d209cde9b04ac98f6b713c52acc
Arvind Rana <arvindrana6969@gmail.com>
2025-02-08 21:36:05 +0545

    Add bubblesort fuck-you.c hello.c

--- bubblesort.c
+++ bubblesort.c
@@ -2,19 +2,25 @@
 
 int main()
 {
-	int list[] = [100, 99, 21, 37, 42, 11];
+	int list[] = {100, 99, 21, 37, 42, 11};
 	int listLength = sizeof(list)/sizeof(list[0]);
 
 	for (int i = 0; i < listLength; i++)
 	{
-		for (int j = 0; j < listLength; j++)
+		for (int j = 0; j < listLength - 1; j++)
 		{
-			if (list[i] > list[i + 1])
+			if (list[j] > list[j + 1])
 			{
-				int temp = list[i];
-				list[i] = list[i + 1];
-				list[i+1] = list[i];
+				int temp = list[j];
+				list[j] = list[j + 1];
+				list[j+1] = temp;
 			}
 		}
 	}
+
+	for (int i = 0; i < listLength; i++)
+	{
+		printf("%d\t", list[i]);
+	}
+	printf("\n");
 }
